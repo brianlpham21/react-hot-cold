bcfdf60fe13dcc2430cdde9c57ecbdb97bf32eb1
Brian Pham <brianlpham21@gmail.com>
2018-02-28 18:46:34 -0800

    converted to Redux use

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 
 import './reset.css';
 import './index.css';
 
 import Game from './components/game';
 
+import store from './store';
+
 ReactDOM.render(
-  <Game />,
+  <Provider store={store}>
+    <Game />
+  </Provider>,
   document.getElementById('root')
 );
